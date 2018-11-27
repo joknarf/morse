8f8bad7697f591d84df482c2a3b74d5024e04dae
joknarf <joknarf@free.fr>
2018-11-27 15:16:30 +0100

    Morse code. Failing tests. Pending Jenkins configuration

---
+++ morse.py
@@ -0,0 +1,9 @@
+# morse.py
+# pylint: disable=missing-docstring
+
+class Morse:
+    def __init__(self, code):
+        self.code = code
+
+    def decode(self, message):
+        pass # TODO: implement the behavior!
